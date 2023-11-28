962fb9a71c443198695f824007d29bd6f35602af
mizhgun <m.a.ugryumov@outlook.com>
2023-11-29 02:04:07 +0300

    done laba 1

---
+++ postgres_config.py
@@ -0,0 +1,8 @@
+postgres_config = {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'glasses',
+    'USER': 'mizhgun',
+    'PASSWORD': 'MA253641u_',
+    'HOST': 'localhost',
+    'PORT': '5432',
+}
