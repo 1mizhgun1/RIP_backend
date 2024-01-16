36ee952b245543cdbe08faa16d9bf352bff3efc3
mizhgun <m.a.ugryumov@outlook.com>
2024-01-16 18:47:06 +0300

    fix

--- glasses_api/models.py
+++ glasses_api/models.py
@@ -29,7 +29,7 @@ class OpticOrder(models.Model):
     closed = models.DateTimeField(verbose_name="Закрытие", null=True, blank=True)
     status = models.CharField(max_length=1, verbose_name="Статус", default='I') # I - inputing, P - processing, D - deleted by user, A - success, W - fail
     user = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name="Пользователь", related_name="user")
-    moderator = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name="Модератор", related_name="moderator")
+    moderator = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name="Модератор", related_name="moderator", null=True, blank=True)
 
 # I --- P --- A
 #  \     \
